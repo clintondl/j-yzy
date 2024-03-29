81ec8630dc9881c8577c940cdcf0c31dc547f94f
Obiora Igboanusi <66188726+obioraigboanusi@users.noreply.github.com>
2024-03-29 15:37:13 +0100

    Resolve hero styles

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -3,21 +3,21 @@ import { coinTensorOffers } from "../utils/dummyData";
 
 function Hero() {
   return (
-    <section className="">
-      <div className="container">
-        <h1 className="font-medium text-4xl lg:text-[54px] text-center lg:w-3/5 mx-auto pt-[52px] lg:leading-[60px]">
+    <section className="layout-pd">
+      <div className="container flex flex-col items-center text-center">
+        <h1 className="font-medium text-4xl lg:text-[54px]  mb-[32px]">
           Make informed decisions, transact securely, and earn passively
         </h1>
-        <p className="text-sm font-normal text-center lg:w-2/5 mx-auto pt-8 pb-4 lg:pb-8">
+        <p className="text-sm text-faint-60 mb-[49px]">
           We&apos;re revolutionizing DeFi with the power of AI-driven insights,
           secure DePIN transactions, and a rewarding revenue-sharing model.
         </p>
         <img
           src={rock}
           alt="rock"
-          className="lg:h-[300px] max-h-full block mx-auto pb-12 pt-6 lg:pt-12"
+          className=" "
         />
-        <div className="flex-box lg:justify-between  arc-border rectangle bg-[#FFFFFF0D] lg:p-10">
+        <div className="flex-box lg:justify-between  arc-border rectangle bg-[#FFFFFF0D] lg:p-10 mt-[95px]">
           {coinTensorOffers.map((offer) => (
             <div key={offer.heading} className="flex gap-5 lg:w-[30%] p-5">
               <div>{offer.icon}</div>
