485322ba2cec2d518a848a4d0530fda587aa67fe
Obiora Igboanusi <66188726+obioraigboanusi@users.noreply.github.com>
2024-03-29 18:00:09 +0100

    resolve how it works styles

--- src/component/TechnologyBehind.jsx
+++ src/component/TechnologyBehind.jsx
@@ -4,25 +4,27 @@ function TechnologyBehind() {
   return (
     <section className="py-16 relative">
       <div className="container">
-        <div className="number absolute w-full lg:text-[200px] font-bold left-0 top-[163px] flex justify-center font-dms z-[1]">
+        <div className="number absolute w-full text-[64px] lg:text-[200px] font-light left-0 top-[163px] flex justify-center font-dms z-[1]">
           CoinTensor AI
         </div>
         <h2 className="text-center heading-2">
           <span className="lg:block">The Technology Behind </span>
           <span className="lg:block lg:mt-5">CoinTensor AI</span>
         </h2>
-        <div className="mt-[106px] p-[47px] flex-box  mx-auto justify-between bg-[url(./assets/circular-bg.png)] bg-no-repeat bg-center bg-cover max-w-[978px]">
-          {aboutStatement.map(({ heading, content }) => (
-            <div
-              key={heading}
-              className="border border-[#FFFFFF0D] p-[32px] lg:w-[40%] bg-[#0000009a]"
-            >
-              <h3 className="text-[22px]  lg:text-4xl leading-6 font-medium mt-2 lg:w-3/5">
-                {heading}
-              </h3>
-              <p className="py-3 primary-text">{content}</p>
-            </div>
-          ))}
+        <div className="mt-[87px] lg:mt-[106px] lg:h-[470px] bg-[url(./assets/image.png)] lg:bg-[url(./assets/circular-bg.png)] bg-no-repeat bg-center bg-contain lg:bg-cover lg:max-w-[978px] bg-clip-content bg-origin-content mx-auto ">
+          <div className="h-full flex-box lg:items-center space-y-[160px] lg:space-y-[0] justify-between ">
+            {aboutStatement.map(({ heading, content }) => (
+              <div
+                key={heading}
+                className="border border-gray-800 p-[24px] lg:p-[32px] lg:w-[40%] lg:max-w-[340px] bg-[#000000b0] min-h-[249px] lg:min-h-[300px]"
+              >
+                <h3 className="text-[22px] lg:text-4xl leading-6 font-medium ">
+                  {heading}
+                </h3>
+                <p className="py-3 primary-text">{content}</p>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </section>
