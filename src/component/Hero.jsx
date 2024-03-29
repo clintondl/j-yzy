2063dc076e854928b4ad210f0f2334979f6bd82e
Obiora Igboanusi <66188726+obioraigboanusi@users.noreply.github.com>
2024-03-29 19:50:04 +0100

    Activate react particles

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -5,7 +5,7 @@ import ParticlesBg from "./ParticlesBg";
 function Hero() {
   return (
     <section className="layout-pd relative">
-      <div className="absolute top-0 bottom-0 right-0 ">
+      <div className="absolute top-0 bottom-0 right-0 z-[50]">
         <ParticlesBg />
       </div>
       <div className="container flex flex-col items-center text-center">
