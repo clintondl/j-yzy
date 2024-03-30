fa135f6fa4ee7a31308f68a767c0e68547316de9
Obiora Igboanusi <66188726+obioraigboanusi@users.noreply.github.com>
2024-03-30 05:59:35 +0100

    roadmap mobile bg

--- src/component/RoadMap.jsx
+++ src/component/RoadMap.jsx
@@ -12,12 +12,12 @@ function RoadMap() {
             </p>
           </header>
           <div className="max-w-[1440px] mx-auto mt-10 road-map w-full lg:h-[356px] lg:flex lg:items-center">
-            <div className="flex flex-col lg:flex-row gap-[16px] items-center lg:items-stretch container">
+            <div className="flex flex-col lg:flex-row lg:justify-center gap-[16px] items-center lg:items-stretch container">
               {roadMap.map(({ heading, content }, i) => (
                 <div
                   key={heading}
                   className={[
-                    "border border-[#ffffff4c] p-4 bg-[#000000] max-w-[276.5px]",
+                    "border border-[#ffffff4c] p-4 bg-[#000000] lg:w-[calc(20%)] xl:w-[calc(25%-16px)] max-w-[276.5px]",
                     i === 0 ? "" : "border-dashed ",
                   ].join(" ")}
                 >
@@ -38,25 +38,3 @@ function RoadMap() {
 }
 
 export default RoadMap;
-/* Frame 7 */
-
-/* Auto layout */
-// display: flex;
-// flex-direction: column;
-// align-items: flex-start;
-// padding: 24px;
-// gap: 16px;
-
-// width: 276.5px;
-// height: 198px;
-
-// background: #000000;
-// background: color(display-p3 0.000 0.000 0.000);
-// border: 1px dashed #ffffff4c;
-// border: 1px dashed color(display-p3 1.000 1.000 1.000 / 0.3);
-
-// /* Inside auto layout */
-// flex: none;
-// order: 2;
-// align-self: stretch;
-// flex-grow: 0;
