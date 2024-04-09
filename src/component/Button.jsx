edab8b0cf5a45bf77f1b5533e8322175e7b67371
Obiora Igboanusi <66188726+obioraigboanusi@users.noreply.github.com>
2024-04-09 11:29:19 +0100

    Action button with arc

---
+++ src/component/Button.jsx
@@ -0,0 +1,10 @@
+
+function Button() {
+  return (
+    <div className="arced bg-[#0f0f0f]">
+      <button className="btn w-full">Stake Now</button>
+    </div>
+  );
+}
+
+export default Button;
--- src/component/staking/rewards/Rewards.jsx
+++ src/component/staking/rewards/Rewards.jsx
@@ -9,8 +9,10 @@ function Rewards() {
         <h2 className="font-medium mb-[24px] text-2xl lg:text-[32px]">
           Rewards
         </h2>
-        <div className="bg-[#FFFFFF0F] w-full">
-          {isStaked ? <Staked /> : <NotStaked />}
+        <div className="arced bg-black">
+          <div className="bg-[#FFFFFF0F] w-full">
+            {isStaked ? <Staked /> : <NotStaked />}
+          </div>
         </div>
       </div>
     </section>
