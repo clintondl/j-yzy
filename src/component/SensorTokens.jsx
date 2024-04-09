11200db9dd6cf62fedd17b951c18c3e51be261a4
Obiora Igboanusi <66188726+obioraigboanusi@users.noreply.github.com>
2024-04-09 09:05:10 +0100

    Add clickable Dexscreener and Dextools logos

--- src/component/SensorTokens.jsx
+++ src/component/SensorTokens.jsx
@@ -1,3 +1,6 @@
+import dexscreener from "../assets/dexscreener-logo.svg";
+import dextools from "../assets/dextools_logo.png";
+
 function SensorTokens() {
   return (
     <section id="trade" className="py-16">
@@ -9,7 +12,7 @@ function SensorTokens() {
             announcements on our website and social media channels to
             participate in the future of DeFi.
           </p>
-          <div className="flex justify-center py-[35px]">
+          <div className="flex justify-center mt-[35px] mb-[20px]">
             <a
               href="https://app.uniswap.org/swap?outputCurrency=0x1ddbB18ECf92d02Bb386224F0d160f30305150dD&chain=ethereum"
               target="_blank"
@@ -17,6 +20,30 @@ function SensorTokens() {
               className="btn arc-border w-fit"
             >
               Buy Token
+            </a>
+          </div>
+          <div className="flex items-center justify-center gap-[20px]">
+            <a
+              href="https://dexscreener.com/ethereum/0x99D96F99309FC5EBe4fC534D104BA71DB0912ae1"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={dexscreener}
+                alt="dexscreener"
+                className="w-[20px] min-w-[20px]"
+              />
+            </a>
+            <a
+              href="https://www.dextools.io/app/en/ether/pair-explorer/0xf5c92780138061a113fd708d4b403e0e830effff"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={dextools}
+                alt="dexscreener"
+                className="w-[30px] min-w-[30px]"
+              />
             </a>
           </div>
         </div>
